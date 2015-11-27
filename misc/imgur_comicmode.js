cecb852cb6d3813d00ef834520647ac4a7848ac3
Martín (Netux) Rodríguez <Netox005@users.noreply.github.com>
2015-11-27 16:22:51 -0300

    Create imgur_comicmode.js

---
+++ misc/imgur_comicmode.js
@@ -0,0 +1,5 @@
+var linkEl = document.createElement('link');
+linkEl.rel = 'stylesheet';
+linkEl.type = 'text/css';
+linkEl.href = 'https://rawgit.com/Netox005/netox005.github.io/master/misc/imgur_comicmode.css';
+document.getElementsByTagName('head')[0].appendChild(linkEl);
